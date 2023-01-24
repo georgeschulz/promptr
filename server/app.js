aced2d0fef682bb3e02eb83e6af0769efb26aad9
97812180+georgeschulz@users.noreply.github.com <97812180+georgeschulz@users.noreply.github.com>
2023-01-24 18:15:13 -0500

    Add route to access static build directory

--- server/app.js
+++ server/app.js
@@ -13,6 +13,20 @@ app.get('/test', async (req, res) => {
     res.status(200).send('test')
 });
 
+//general path for getting static pages
+app.get("/*", cors({ credentials: true, origin: 'http://localhost:3000' }), (req, res) => {
+    if(process.env.NODE_ENV === 'production') {
+        res.sendFile(path.join(__dirname, "../client/build", "index.html"), (err) => {
+            if(err) {
+                console.log('Incorrect path');
+                res.status(500).send()
+            }
+        });
+    } else {
+        res.redirect('https://localhost:3000/');
+    }
+})
+
 app.listen(port, () => {
     console.log(`Example app listening at ${port}`);
 })
